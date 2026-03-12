ffc3e72c6e1e2bd886da411a17b17ad7d02f86c3
CatPimentel <seu-email-do-github@example.com>
2026-03-12 09:05:16 -0300

    feat: ntrodução finalizada do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ExemploComponentComponent } from './_components/exemplo-component/exemplo-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExemploComponentComponent
   ],
   imports: [
     BrowserModule,
