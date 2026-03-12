0b6c41254f229384139db922c59ad17bdbd9aa70
CatPimentel <seu-email-do-github@example.com>
2026-03-12 10:21:24 -0300

    feat: fase 01 concluída

---
+++ src/app/_components/base-ui/base-ui.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AppRoutingModule } from "../../app-routing.module";
+
+@Component({
+  selector: 'app-base-ui',
+  templateUrl: './base-ui.component.html',
+  styleUrl: './base-ui.component.css'
+})
+export class BaseUiComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ExemploComponentComponent } from './_components/exemplo-component/exemplo-component.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { FooterComponent } from './_components/footer/footer.component';
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ExemploComponentComponent
+    NavbarComponent,
+    FooterComponent,
+    BaseUiComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
